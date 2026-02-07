650668a21817a25e24e7915e836202df22c9d2a2
Oliver Batey <oliver.batey@deliveroo.co.uk>
2026-02-07 16:56:31 +0000

    Removed node_id from TokenNode; completws TokenSequence; pair_counts and pair_occurences to TokenSequenceRegister

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -3,34 +3,23 @@ import cProfile
 import regex as re
 
 from collections import defaultdict, Counter
-from typing import BinaryIO, Iterable
+from typing import BinaryIO, Callable, Iterable
 
-class TokenSequenceRegister:
-    def __init__(self, pretoken_counts: dict[tuple[int, ...], int]):
-        self.pretoken_counts = pretoken_counts
-        self.sequence_weights: dict[int, int] = dict()
-        self.sequence_tokens: dict[int, tuple[int, ...]] = dict()
 
-    def _build_sequence_registry(self):
-        for seq_id, sequence in enumerate(self.pretoken_counts):
-            self.sequence_weights[seq_id] = self.pretoken_counts[sequence]
-            self.sequence_tokens[seq_id] = sequence
+class TokenNode:
+    def __init__(self, symbol_id: int, seq_id: int):
+        self.symbol_id = symbol_id  # This is the id of the TokenNode in the vocabulary.
+        self.seq_id = seq_id  # An id for the TokenSequence that the TokenNode belongs to.
+        self.next = None
+        self.prev = None
+        self.alive = True
 
-    def _validate_registry(self):
-        assert len(self.pretoken_counts) == len(self.sequence_weights), "Registry has a different number of elements than the pretoken counts"
-        assert sum(self.sequence_weights.values()) == sum(self.pretoken_counts.values())
-        for seq_id, sequence in self.sequence_tokens.items():
-            assert self.sequence_weights[seq_id] == self.pretoken_counts[sequence]
-
-    def build_sequence_registry(self):
-        self._build_sequence_registry()
-        self._validate_registry()
 
 class TokenSequence:
     def __init__(self, seq_id: int):
         """Initialise a TokenSequence with sentinel TokenNodes"""
-        self.head: TokenNode = TokenNode(symbol_id=None, node_id=None, seq_id=seq_id)
-        self.tail: TokenNode = TokenNode(symbol_id=None, node_id=None, seq_id=seq_id)
+        self.head: TokenNode = TokenNode(symbol_id=None, seq_id=seq_id)
+        self.tail: TokenNode = TokenNode(symbol_id=None, seq_id=seq_id)
         self.head.prev=None
         self.head.next=self.tail
         self.tail.prev=self.head
@@ -43,30 +32,129 @@ class TokenSequence:
             yield s
             s = s.next
 
-    def insert_at_head(self, s: TokenNode):
-        new_node = TokenNode
-        new_node.next = self.head
-        self.head.prev = new_node
-        self.head = new_node
+    def insert_at_head(self, token_node: TokenNode):
+        current_first_node = self.head.next
+        token_node.next = current_first_node
+        token_node.prev = self.head
+        self.head.next = token_node
+        current_first_node.prev = token_node
 
-    def insert_at_tail(self, s: TokenNode):
-        ...
 
-    def merge_at(self, s: TokenNode, new_symbol: TokenNode):
-        ...
+    def insert_at_tail(self, token_node: TokenNode):
+        current_last_node = self.tail.prev
+        token_node.prev = current_last_node
+        token_node.next = self.tail
+        self.tail.prev = token_node
+        current_last_node.next = token_node
+
+    def _validate_left_node(self, node: TokenNode):
+        assert node is not None
+        assert node.alive is True
+        assert node is not self.head and node is not self.tail
+        assert node.prev is not None and node.next is not None
+
+    def _validate_right_node(self, node: TokenNode):
+        assert node is not None
+        assert node.alive is True
+        assert node is not self.tail
+        assert node.next is not None
+
+    def _sever_node(self, node: TokenNode):
+        node.alive = False
+        node.next, node.prev = None, None
+
+    def merge_at(self, left: TokenNode, symbol_id: int) -> tuple:
+        """Merge pairs of TokenNode using the left node as a reference
+
+        Merges the sequence X <-> A <-> B <-> Y into X <-> C <-> Y
+        """
+        self._validate_left_node(left)
+        A = left
+        X = A.prev
+        B = A.next
+        self._validate_right_node(B)
+        Y = B.next
+        C = TokenNode(symbol_id=symbol_id, seq_id=self.seq_id,)
+        X.next = C
+        C.prev = X
+        C.next = Y
+        Y.prev = C
+        self._sever_node(A)
+        self._sever_node(B)
+        return X, C, Y
 
     @classmethod
-    def from_tokens(self, seq_id: int, tokens: Iterable[int]):
+    def from_tokens(cls, seq_id: int, tokens: Iterable[int], node_factory: Callable):
+        sequence = cls(seq_id=seq_id)
+        for token in tokens:
+            node = node_factory(symbol_id=token, seq_id=seq_id)
+            sequence.insert_at_tail(node)
+        return sequence
+
+    def symbol_counts(self):
+        symbol_counts = defaultdict(int)
+        for token_node in self:
+            if token_node.symbol_id:
+                symbol_counts[token_node.symbol_id] +=1
+        return symbol_counts
+
+    def total_node_weights(self):
+        return sum(self.symbol_counts().values())
+
+
+
+class TokenSequenceRegister:
+    def __init__(self, pretoken_counts: dict[tuple[int, ...], int]):
+        self.pretoken_counts = pretoken_counts
+        self.sequence_weights: dict[int, int] = dict()
+        self.sequence_tokens: dict[int, TokenSequence] = dict()
+        self.pair_counts: dict[tuple[int, int], int] = defaultdict(int)
+        self.pair_occurrences: dict[tuple[int, int], list[TokenNode]] = defaultdict(list)
+        self.build_sequence_registry()
+
+    def _create_token_node(self, symbol_id: int, seq_id, **kwargs) -> TokenNode:
+        return TokenNode(symbol_id=symbol_id, seq_id=seq_id)
+
+    def _build_sequence_registry(self):
+        for seq_id, sequence in enumerate(self.pretoken_counts):
+            self.sequence_weights[seq_id] = self.pretoken_counts[sequence]
+            self.sequence_tokens[seq_id] = TokenSequence.from_tokens(
+                seq_id=seq_id, tokens=sequence, node_factory=self._create_token_node
+            )
+
+    def _build_pair_counts(self):
+        for k, v in self.pretoken_counts.items():
+            for j in range(len(k) - 1):
+                pair = (k[j], k[j + 1])
+                self.pair_counts[pair] += v
+
+    def _build_pair_occurances(self):
+        for seq_id, token_seq in self.sequence_tokens.items():
+            for token in token_seq:
+                if (token.next is not token_seq.tail) and (token.alive and token.next.alive):
+                    self.pair_occurrences[(token.symbol_id, token.next.symbol_id)].append(token)
+
+
+    # TODO: update validation to expect linked list structure
+    def _validate_build_sequence_registry(self):
+        assert len(self.pretoken_counts) == len(self.sequence_tokens), "Registry has a different number of elements than the pretoken counts"
+
+    def _validate_build_pair_counts(self):
         ...
 
-class TokenNode:
-    def __init__(self, node_id: int, symbol_id: int, seq_id: int):
-        self.node_id = node_id  # Unique id for the node.
-        self.symbol_id = symbol_id  # This is the id of the TokenNode in the vocabulary.
-        self.seq_id = seq_id  # An id for the TokenSequence that the TokenNode belongs to.
-        self.next = None
-        self.prev = None
-        self.alive = True
+    def _validate_build_pair_occurances(self):
+        ...
+
+    def _validate(self):
+        self._validate_build_sequence_registry()
+        self._validate_build_pair_counts()
+        self._validate_build_pair_occurances()
+
+    def build_sequence_registry(self):
+        self._build_sequence_registry()
+        self._build_pair_counts()
+        self._build_pair_occurances()
+        self._validate()
 
 
 def find_chunk_boundaries(
@@ -325,31 +413,20 @@ def train_bpe(
     return train(pretoken_counts, vocab_size, special_tokens)
 
 
-def build_sequence_registry(
-    pretoken_counts: dict[tuple[int, ...], int]
-) -> tuple[dict[int, int], dict[int, tuple[int, ...]]]:
-    sequence_weight = defaultdict(int)
-    sequence_tokens = dict()
-    for seq_id, sequence in enumerate(pretoken_counts):
-        sequence_weight[seq_id] += pretoken_counts[sequence]
-        sequence_tokens[seq_id] = sequence
-    return sequence_weight, sequence_tokens
-
-
-
-
-
 if __name__ == "__main__":
     path = "data/TinyStoriesV2-GPT4-valid.txt"
     vocab_size=1000
     special_tokens = ["<|endoftext|>"]
     pretoken_counts = build_pretoken_counts(path=path, special_tokens=special_tokens)
     register = TokenSequenceRegister(pretoken_counts)
-    register.build_sequence_registry()
     sequence_weights = register.sequence_weights
     sequence_tokens = register.sequence_tokens
+    pair_counts = register.pair_counts
+    pair_occurrences = register.pair_occurrences
 
-    cProfile.run(
-        "train_bpe(path, vocab_size, special_tokens)",
-        sort="cumtime",
-    )
+
+    breakpoint()
+    # cProfile.run(
+    #     "train_bpe(path, vocab_size, special_tokens)",
+    #     sort="cumtime",
+    # )
